7a9969537660ac65536c628ef8c529ce60a127f0
Zackary S <zackaria.celimane@gmail.com>
2023-03-01 01:13:40 +0100

    added netlify form handler

--- components/sections/TheContact.vue
+++ components/sections/TheContact.vue
@@ -9,7 +9,7 @@
 				</h2>
 			</div>
 			<div class="mt-12">
-				<form @submit.prevent="formSubmit" id="contact-form"
+				<form netlify @submit.prevent="formSubmit" id="contact-form"
 					class="grid grid-cols-1 gap-y-6 sm:grid-cols-2 sm:gap-x-8">
 					<div>
 						<label for="first-name" class="block text-sm font-medium text-gray-50">Name</label>
