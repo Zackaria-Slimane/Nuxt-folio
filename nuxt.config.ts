3c65473aaf81faa12fd4089c34cd121b35f49f1c
Zackary S. <zackaria.celimane@gmail.com>
2023-08-23 14:21:04 +0100

    updated site url

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,44 +1,38 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
 	modules: [
-		'@nuxtjs/tailwindcss',
-		'@pinia/nuxt',
-		'nuxt-simple-sitemap',
-		'nuxt-simple-robots',
-		'@nuxtjs/color-mode'
+		"@nuxtjs/tailwindcss",
+		"@pinia/nuxt",
+		"nuxt-simple-sitemap",
+		"nuxt-simple-robots",
+		"@nuxtjs/color-mode",
 	],
 	colorMode: {
-		classSuffix: ''
+		classSuffix: "",
 	},
 	tailwindcss: {
 		exposeConfig: false,
 		cssPath: "@/assets/css/tailwind.css",
-		configPath: 'tailwind.config.js',
+		configPath: "tailwind.config.js",
 		injectPosition: 0,
 		viewer: true,
 	},
 	runtimeConfig: {
 		public: {
-			titleSeparator: '|',
+			titleSeparator: "|",
 			trailingSlash: true,
-			siteUrl: process.env.NUXT_PUBLIC_SITE_URL || 'https://zackariasl.dev/',
-			siteName: 'Zackaria SLIMANE - Frontend developer',
-			siteDescription: 'Zackaria SLIMANE - Front-end Web Developer portfolio',
-			language: 'en-US', // prefer more explicit language codes like `en-AU` over `en`
+			siteUrl: process.env.NUXT_PUBLIC_SITE_URL || "https://zackariasl.ma/",
+			siteName: "Zackaria SLIMANE - Frontend developer",
+			siteDescription: "Zackaria SLIMANE - Front-end Web Developer portfolio",
+			language: "en-US", // prefer more explicit language codes like `en-AU` over `en`
 		},
 		GOOGLE_ANALYTICS_ID: process.env.GOOGLE_ANALYTICS_ID,
 	},
-	css: [
-		"@/assets/css/tailwind.css",
-	],
+	css: ["@/assets/css/tailwind.css"],
 	nitro: {
 		prerender: {
 			crawlLinks: true,
-			routes: [
-				'/',
-				'/about',
-				'/projects'
-			]
+			routes: ["/", "/about", "/projects"],
 		},
 		compressPublicAssets: {
 			gzip: true,
@@ -47,32 +41,44 @@ export default defineNuxtConfig({
 		minify: true,
 	},
 	ssr: true,
-	components: [{
-		path: '~/components', pathPrefix: false,
-	}],
+	components: [
+		{
+			path: "~/components",
+			pathPrefix: false,
+		},
+	],
 	app: {
 		pageTransition: { name: "page", mode: "out-in", type: "transition" },
 		head: {
-			titleTemplate: '%pageTitle %titleSeparator %siteName',
+			titleTemplate: "%pageTitle %titleSeparator %siteName",
 			title: "Zackaria SLIMANE - Front-end Web Developer",
-			viewport: 'width=device-width, initial-scale=1.0',
+			viewport: "width=device-width, initial-scale=1.0",
 			htmlAttrs: {
 				lang: "en-US",
 			},
 			meta: [
 				{ name: "description", content: "Zackaria SLIMANE - Front-end Web Developer portfolio" },
 				{ name: "og:description", content: "Zackaria SLIMANE - Front-end Web Developer portfolio" },
-				{ name: "twitter:description", content: "Zackaria SLIMANE - Front-end Web Developer portfolio" },
+				{
+					name: "twitter:description",
+					content: "Zackaria SLIMANE - Front-end Web Developer portfolio",
+				},
 				{ name: "og:title", content: "Zackaria SLIMANE - Front-end Web Developer" },
 				{ name: "twitter:title", content: "Zackaria SLIMANE - Front-end Web Developer" },
-				{ name: "application-name", content: "Zackaria SLIMANE - Front-end Web Developer portfolio" },
-				{ name: "apple-mobile-web-app-title", content: "Zackaria SLIMANE - Front-end Web Developer" },
+				{
+					name: "application-name",
+					content: "Zackaria SLIMANE - Front-end Web Developer portfolio",
+				},
+				{
+					name: "apple-mobile-web-app-title",
+					content: "Zackaria SLIMANE - Front-end Web Developer",
+				},
 				{ charset: "UTF-8" },
 				//{ name: 'color-scheme', content: 'dark light' }
 			],
 			link: [
 				{ rel: "icon", href: "./favicon.ico" },
-				{ rel: "canonical", href: "https://zackariasl.dev" },
+				{ rel: "canonical", href: "https://zackariasl.ma" },
 				{ rel: "manifest", href: "./site.webmanifest" },
 				{ rel: "apple-touch-icon", sizes: "180x180", href: "./apple-touch-icon.png" },
 				{ rel: "icon", type: "image/png", sizes: "32x32", href: "./favicon-32x32.png" },
@@ -82,15 +88,19 @@ export default defineNuxtConfig({
 			script: [
 				{
 					async: true,
-					src: 'https://www.googletagmanager.com/gtag/js?id=G-VRCYTH918H',
-				}, {
-					async: true, children: ` window.dataLayer = window.dataLayer || [];
+					src: "https://www.googletagmanager.com/gtag/js?id=G-VRCYTH918H",
+				},
+				{
+					async: true,
+					children: ` window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
   gtag('config', 'G-VRCYTH918H',{
   cookie_flags: 'max-age=7200;secure;samesite=none'
-});` }],
+});`,
+				},
+			],
 		},
 	},
-})
+});
